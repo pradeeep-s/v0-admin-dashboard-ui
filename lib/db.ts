56328817fc58faf78ed4c7b2e5b5840625f39a6b
Pradeep S <62656333+pradeeep-s@users.noreply.github.com>
2026-05-19 12:01:57 +0530

    Update database connection logic in db.ts
    Refactor database connection to use DATABASE_URL and resolve hostname to IPv4.

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,25 @@
+import "server-only";
 import { Pool } from "pg"
+import dns from "dns/promises"
+import dotenv from "dotenv"
 
-export const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  port: 5432,
+dotenv.config()
 
- 
-})
+// Force Node to resolve IPv4 first
+dns.setDefaultResultOrder("ipv4first")
+const dbUrl = new URL(process.env.DATABASE_URL!)
+
+  // Resolve hostname to IPv4
+const ipv4Address = (await dns.lookup(dbUrl.hostname, { family: 4 })).address
+const pool = new Pool({
+    host: ipv4Address,
+    port: Number(dbUrl.port) || 5432,
+    user: dbUrl.username,
+    password: dbUrl.password,
+    database: dbUrl.pathname.slice(1), // remove leading '/'
+    max: 10,
+    idleTimeoutMillis: 30000,
+});
+
+export default pool;
+
